c42e54bccc4852da8faa9475e2ba837c993cd4aa
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2017-08-09 16:37:17 +0100

    Add files via upload
    Uploaded the Project.

---
+++ SearchFeature/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SearchFeature</title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/bootstrap-theme.min.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <br />
+        <div class="row">
+            <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12 col-lg-push-7 col-md-push-7 col-sm-push-6">
+                <form class="form-group form-horizontal" id="Form" runat="server">
+                    <div class="input-group">
+                        <asp:label class="control-label form-horizontal input-group-addon" runat="server">Search:</asp:label>
+                        <asp:textBox class="form-control form-horizontal" runat="server" id="SearchTextBox" />
+                    </div>
+                </form>
+            </div>
+        </div>
+        <br />
+        <div class="row" id="DisplayItems" runat="server">
+            <!-- This is where your search results turn up.  Don't add anything here. -->
+        </div>
+        <hr />
+    </div>
+</body>
+<script src="Scripts/jquery-3.1.1.min.js"></script>
+<script src="Scripts/bootstrap.min.js"></script>
+</html>
---
+++ SearchFeature/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public static string JSONString = @"{
+    ""SearchItem"": [
+        {
+            ""ItemSearchTerm"": ""INSERT SEARCH TERM HERE"",
+            ""ItemContent"": ""INSERT SEARCH DATA HERE""
+        },
+        {
+            ""ItemSearchTerm"": ""INSERT SEARCH TERM HERE"",
+            ""ItemContent"": ""INSERT SEARCH DATA HERE""
+        }
+    ]
+}"; //Here's where you store your JSON record of the data you are searching.
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack && !string.IsNullOrWhiteSpace(SearchTextBox.Text)) //It the user has POSTed data, and filled the text box.
+        {
+            DisplayItems.InnerHtml = @"<p>Your Search Results:</p>"; //Clear the results div and give it a title.
+            dynamic JSONObject = JsonConvert.DeserializeObject(JSONString); //create a dynamic object to hold the JSON array
+            for (var Iterator = 0; Iterator < JSONObject.SearchItem.Count; Iterator++)
+            {
+                if (string.Equals(SearchTextBox.Text, JSONObject.SearchItem[Iterator].ItemSearchTerm.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                {
+                    DisplayItems.InnerHtml += @"<p>" 
+                                            + JSONObject.SearchItem[Iterator].ItemContent.ToString() 
+                                            + @"</p>"; //Insert JSON dependent content here.
+                }
+            }
+        }
+        else if (IsPostBack) //Catch if the user clears the search box.
+        {
+            DisplayItems.InnerHtml = @"<!-- This is where your search results turn up.  Don't add anything here. -->"; //Return the results div to it's default value.
+        }
+    }
+}
